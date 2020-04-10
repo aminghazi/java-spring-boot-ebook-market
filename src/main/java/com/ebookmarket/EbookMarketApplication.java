c1afd6a151fb49355d852cf18e7dfa5cc2f80b55
aminghazi <aminghazi@outlook.com>
2020-04-10 18:47:02 +0430

    initial project

---
+++ src/main/java/com/ebookmarket/EbookMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ebookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ebookmarket/EbookMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebookmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
