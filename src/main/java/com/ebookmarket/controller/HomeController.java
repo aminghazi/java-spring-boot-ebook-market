6b74f375a134aed325dd8cea87ebfb5bc61512c1
aminghazi <aminghazi@outlook.com>
2020-04-18 21:36:13 +0430

    adding profile page

--- src/main/java/com/ebookmarket/controller/HomeController.java
+++ src/main/java/com/ebookmarket/controller/HomeController.java
@@ -1,15 +1,31 @@
 package com.ebookmarket.controller;
 
+import com.ebookmarket.domain.User;
 import com.ebookmarket.domain.security.PasswordRestToken;
+import com.ebookmarket.service.UserService;
+import com.ebookmarket.service.impl.UserSecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sun.plugin.liveconnect.SecurityContextHelper;
 
 import java.util.Locale;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
+
     @RequestMapping("/")
     public String index() {
         return "index";
@@ -21,9 +37,29 @@ public class HomeController {
     }
 
     @RequestMapping("/newAccount")
-    public String newAccount(Model model) {
-        model.addAttribute("classActiveNewAccount", true);
-        return "account";
+    public String newAccount(
+            Locale locale,
+            @RequestParam("token") String token,
+            Model model) {
+        PasswordRestToken passwordRestToken = userService.getPasswordRestToken(token);
+
+        if (passwordRestToken == null) {
+            String message = "Invalid Token.";
+            model.addAttribute("message", message);
+            return "redirect:/badRequest";
+        }
+
+        User user = passwordRestToken.getUser();
+        String username = user.getUsername();
+
+        UserDetails userDetails = userSecurityService.loadUserByUsername(username);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        model.addAttribute("classActiveEdit", true);
+        return "myProfile";
     }
 
     @RequestMapping("/login")
@@ -33,11 +69,8 @@ public class HomeController {
     }
 
     @RequestMapping("/forgetPassword")
-    public String forgetPassword(
-            Locale locale,
-            @RequestParam("token") String token,
-            Model model) {
-        PasswordRestToken passwordRestToken = userService.getPasswordRestToken(token);
+    public String forgetPassword(Model model) {
+
         model.addAttribute("classActiveForgetPassword", true);
         return "account";
     }
